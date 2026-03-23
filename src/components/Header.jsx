a6dac6563a33861bf59ca25c2caa20a378bee3b7
youssef1365 <alkarimyoussef@gmail.com>
2026-03-23 14:06:47 +0100

    Lastfixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -139,30 +139,32 @@ const Header = ({ activeSection, scrollVelocity, scrollDirection, theme: themePr
     return unsubscribe;
   }, [scrollProgress]);
 
+  const scrollYRef = useRef(0);
+
   useEffect(() => {
     if (mobileMenuOpen) {
       scrollYRef.current = window.scrollY;
+
       document.body.style.position = 'fixed';
       document.body.style.top = `-${scrollYRef.current}px`;
       document.body.style.left = '0';
       document.body.style.right = '0';
-      document.body.style.overflow = 'hidden';
       document.body.style.width = '100%';
     } else {
       document.body.style.position = '';
       document.body.style.top = '';
       document.body.style.left = '';
       document.body.style.right = '';
-      document.body.style.overflow = '';
       document.body.style.width = '';
+
       window.scrollTo(0, scrollYRef.current);
     }
+
     return () => {
       document.body.style.position = '';
       document.body.style.top = '';
       document.body.style.left = '';
       document.body.style.right = '';
-      document.body.style.overflow = '';
       document.body.style.width = '';
     };
   }, [mobileMenuOpen]);
